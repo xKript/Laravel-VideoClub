26701958761e6c09088dd2ffef7e3c0e2baaf252
xCast <xleed@protonmail.ch>
2019-11-02 21:54:54 -0500

    minor changes

--- commands.sh
+++ commands.sh
@@ -1,6 +1,6 @@
+php artisan make:model Movie
 php artisan migrate:install
 php artisan make:migration create_movies_table --create=movies
-
 
 composer require laravel/ui --dev
 php artisan ui vue --auth
